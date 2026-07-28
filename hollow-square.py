f597a8e3c24bb01d1c94daf8ec18cfd3c403e5f0
Gouda Ananya <ananyagouda25@gmail.com>
2026-07-28 11:50:03 +0530

    Add hollow square printing program
    This program prints a hollow square based on user input.

---
+++ hollow-square.py
@@ -0,0 +1,9 @@
+## Program: Hollow square
+num = int(input("Enter a number:"))
+if num==1:
+    print("*")
+else:
+    print("*"*num)
+    for _ in range(num-2):
+        print("*"+" "*(num-2)+"*")
+    print("*"*num)
